f4cea9618d8a8dfd4fafba84a24fea57341184df
emafina <emanuele.finazzi@gmail.com>
2024-04-17 04:56:32 +0000

    chore: function structure added

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+
+PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"
+
+SEARCH() {
+  
+}
+
+LOGIN() {
+  echo "Enter your username:"
+  read NAME
+}
 RN=$(( 1+RANDOM%1000 ))
 echo $RN
+
